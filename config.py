212dae497169638ab275d2f1682fc6689f2a4a9c
yaoguanfei <yaoguanfei@youmi.net>
2019-06-24 18:58:01 +0800

    update

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 # 此处配置产品名称的中英文映射，主要为解决获取github 的图片url 获取问题
 # 因为发现若存在中文，对应的图片url会解析为其他符号导致之后获取不到正确的url
-Chinese_to_English={"AG":"AG","优投":"youtou","营销云":"yingxiaoyun"}
+Chinese_to_English = {"AG": "AG", "优投": "youtou", "有米云": "youmiyun", "米汇": "mihui"}
