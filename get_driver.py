196010d70849201fbdc096618f6bc77e0df396e8
yaoguanfei <yaoguanfei@youmi.net>
2019-06-27 19:45:15 +0800

    hh

--- get_driver.py
+++ get_driver.py
@@ -1,4 +1,4 @@
-
+import os
 from selenium import webdriver  # 导入selenium,注意不能使用import selenium
 
 
@@ -17,7 +17,6 @@ from selenium import webdriver  # 导入selenium,注意不能使用import seleni
 #     driver = webdriver.Chrome()
 #     driver.implicitly_wait(5)
 #     driver.maximize_window()  # 最大化浏览器
-#     driver.get(url)  # 通过get()方法，打开一个url站点
-
-
+#     driver.get(url)  # 通过get()方法，打开一个url站点'
+os.system("export PATH=‘$PATH:/home/ymserver/tmp/chromedriver’ ")
 driver = webdriver.Chrome()# 初始化一个谷歌浏览器实例：driver
