dc22a5a0810dcb555ca74d52662d421b9afd232f
yaoguanfei <yaoguanfei@youmi.net>
2019-06-28 11:56:11 +0800

    hh

--- get_driver.py
+++ get_driver.py
@@ -1,6 +1,6 @@
 import os
 from selenium import webdriver  # 导入selenium,注意不能使用import selenium
-
+from selenium.webdriver.chrome.options import Options
 
 # if drivername.lower() == "google" or drivername == "chrome":
 # driver = webdriver.Chrome()  # driver = webdriver.Chrome()# 谷歌浏览器 driverChrome 放在了/usr/bin 下,忘记了可使用whereis 查询
@@ -15,6 +15,8 @@ from selenium import webdriver  # 导入selenium,注意不能使用import seleni
 #     driver.implicitly_wait(5)
 #     driver.maximize_window()  # 最大化浏览器
 #     driver.get(url)  # 通过get()方法，打开一个url站点'
-# os.system("export PATH=‘$PATH:/home/ymserver/tmp/chromedriver’ ")
-
-driver = webdriver.PhantomJS()  # 初始化一个无头谷歌浏览器实例：driver
+os.system("export PATH=‘$PATH:/opt/google/chromedriver")
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver = webdriver.PhantomJS()  # 初始化一个无头谷歌浏览器实例：driver,对PhantomJS的Selenium支持已被弃用，请使用无头版Chrome或Firefox
