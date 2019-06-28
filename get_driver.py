991540a6ba0f944bd85c2850a5319207828f4062
yaoguanfei <yaoguanfei@youmi.net>
2019-06-28 10:13:58 +0800

    hh

--- get_driver.py
+++ get_driver.py
@@ -2,7 +2,6 @@ import os
 from selenium import webdriver  # 导入selenium,注意不能使用import selenium
 from selenium.webdriver.chrome.options import Options
 
-
 # if drivername.lower() == "google" or drivername == "chrome":
 # driver = webdriver.Chrome()  # driver = webdriver.Chrome()# 谷歌浏览器 driverChrome 放在了/usr/bin 下,忘记了可使用whereis 查询
 # if drivername.lower() == "firebox":
@@ -11,14 +10,13 @@ from selenium.webdriver.chrome.options import Options
 # driver = webdriver.Firefox()
 
 
-
-
 # def open_url(url):
 #     driver = webdriver.Chrome()
 #     driver.implicitly_wait(5)
 #     driver.maximize_window()  # 最大化浏览器
 #     driver.get(url)  # 通过get()方法，打开一个url站点'
-os.system("export PATH=‘$PATH:/home/ymserver/tmp/chromedriver’ ")
+# os.system("export PATH=‘$PATH:/home/ymserver/tmp/chromedriver’ ")
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(chrome_options=chrome_options)# 初始化一个无头谷歌浏览器实例：driver
+chromedriver = "/home/ymserver/tmp/chromedriver"
+driver = webdriver.Chrome(chromedriver,chrome_options=chrome_options)  # 初始化一个无头谷歌浏览器实例：driver
