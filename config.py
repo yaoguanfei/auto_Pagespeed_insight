4e0ca313e6887dc91e86fd2a7eb238c0199c71f3
yaoguanfei <44428972+yaoguanfei@users.noreply.github.com>
2019-08-01 19:34:06 +0800

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# 此处配置产品名称的中英文映射，主要为解决获取github 的图片url 获取问题
-# 因为发现若存在中文，对应的图片url会解析为其他符号导致之后获取不到正确的url
-Chinese_to_English = {"有米官网": "youmi", "AG通用版": "AppGrowing", "AG电商版": "AppGrowing_ec", "优投海外DSP": "youtouDSP",
-                      "优投海外SSP": "youtouDSP", "有米云官网": "youmiyun", "米汇产品": "mihui"}
